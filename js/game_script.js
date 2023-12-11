1804d900ae4cabaa5cb346cfdeb95a8170f09ebd
Edson Rodrigues <143434065+ImaCod3r@users.noreply.github.com>
2023-12-11 20:30:00 +0100

    Update game_script.js

--- js/game_script.js
+++ js/game_script.js
@@ -2,21 +2,23 @@
 const mario = document.querySelector('.mario');
 const pedra = document.querySelector('.pedra');
 const pontuacao = document.querySelector('.pontos');
+const reiniciar = document.querySelector('.reiniciar');
 
 // 2. Variaveis do jogo
 var pontos = 0;
-var perdeu = false; 
+var perdeu = false;
 var evento = 'keydown';
-var jump_sound = new Audio('assets/Sons/jump_sound.mp3');
-var gameOver_sound = new Audio('assets/Sons/gameOver.mp3')
+var jump_sound = new Audio('assets/sons/jump_sound.mp3');
+var gameOver_sound = new Audio('assets/sons/gameOver.mp3')
 
 // 3. Funcoes 
 
 // 3.1 Funcao que adiciona um Salto ao player
 const jump = () =>{
-
-    mario.classList.add('jump');
+    
     jump_sound.play();
+    mario.classList.add('jump');
+    
 
     setTimeout(() =>{
 
@@ -36,14 +38,16 @@ const atualizarPontuacao =setInterval(() =>{
 
 },50);
 
+
 // 3.3 Funcao que verifica a colisao do player e da pedra
 const loop = setInterval(() =>{
 
     const posicaoPedra = pedra.offsetLeft;
     const posicaoMario = +window.getComputedStyle(mario).bottom.replace('px',' ');
+        
     if(window.innerWidth <= 750){
-            
-        if(posicaoPedra <= 50 && posicaoPedra > 0 && posicaoMario < 50){
+                
+        if(posicaoPedra <= 35 && posicaoPedra > 0 && posicaoMario < 50){
 
             pedra.style.animation = 'none';
             pedra.style.left = `${posicaoPedra}px`;
@@ -53,37 +57,39 @@ const loop = setInterval(() =>{
             mario.src = 'assets/game-over.png';
 
             mario.style.width = '20px';
-            mario.style.marginLeft = '50px';
+            mario.style.marginLeft = '35px';
 
             clearInterval(loop);
             perdeu = true;
+            reiniciar.style.display = 'flex';
             gameOver_sound.play();
-            }
-        
         }
+    }
+    else{
+        if(posicaoPedra <= 70 && posicaoPedra > 0 && posicaoMario < 85){
+
+            pedra.style.animation = 'none';
+            pedra.style.left = `${posicaoPedra}px`;
         
-        else{
-            
-            if(posicaoPedra <= 70 && posicaoPedra > 0 && posicaoMario < 90){
+            mario.style.animation = 'none';
+            mario.style.bottom = `${posicaoMario}px`;
+            mario.src = 'assets/game-over.png';
 
-                pedra.style.animation = 'none';
-                pedra.style.left = `${posicaoPedra}px`;
-    
-                mario.style.animation = 'none';
-                mario.style.bottom = `${posicaoMario}px`;
-                mario.src = 'assets/game-over.png';
+            mario.style.width = '50px';
+            mario.style.marginLeft = '35px';
 
-                mario.style.width = '50px';
-                mario.style.marginLeft = '30px';
-
-                clearInterval(loop);
-                perdeu = true;
-                gameOver_sound.play();
-                }
-           
+            clearInterval(loop);
+            perdeu = true;
+            reiniciar.style.display = 'flex';
+            gameOver_sound.play();
         }
-
+    }
 },10);
+
+// Funcao que atualiza a tela
+function refresh(){
+    location.reload(true);
+}
 
 
 // 4. Bloco condicional que verifica o tamanho da tela e muda o evento
